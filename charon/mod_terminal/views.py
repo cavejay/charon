d42f298a2138899fb827bff137ea0a81ec15d961
Joseph Spearritt <joe.spearritt@live.com>
2015-11-28 15:02:41 +1000

    Merge branch 'master' of https://github.com/cavejay/charon
    TERMINAL SHIT

--- charon/mod_terminal/views.py
+++ charon/mod_terminal/views.py
@@ -7,13 +7,22 @@
 
 from pathlib import Path
 import subprocess
-from flask import render_template, request, session, jsonify, current_app
+import tempfile
+from flask import render_template, request, session, jsonify, current_app, abort
 from . import mod_terminal
 
 @mod_terminal.route('/open', methods=['POST'])
 def terminal_open():
-    p = subprocess.Popen("cmd", shell=True, stdin=subprocess.PIPE)
-
+    # Open new terminal subprocess
+    # NEED MULTIOS SUPPORT!
+    p = subprocess.Popen(['/bin/bash'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1)
+    #(stdout, stderr) = p.communicate(input="echo something".encode())
+    p.stdin.write("echo something".encode())
+    s = p.stdout.readline().decode()
+    return s
+    #p.stdin.write("how even".encode())
+    #s = p.stdout.read()
+    # Give the terminal session a name
     name = 'term1'
     i = 1
     while name in current_app.config['terminal']:
@@ -25,9 +34,17 @@ def terminal_open():
 
 @mod_terminal.route('/<name>', methods=['POST'])
 def terminal_input(name):
-    return ""
+    if name not in current_app.config['terminal']:
+        return abort(400)
+    p = current_app.config['terminal'][name]
+    p.stdin.write("echo something".encode())
+    return p.stdout.read()
 
 @mod_terminal.route('/<name>/close', methods=['POST'])
 def terminal_close(name):
-    app = current_app._get_current_object()
-    return app.config['something']
+    if name not in current_app.config['terminal']:
+        return abort(400)
+    p = current_app.config['terminal'][name]
+    del current_app.config['terminal'][name]
+    p.kill()
+    return "DONE"
