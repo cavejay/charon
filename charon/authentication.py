ed92c4f85841844f633e8ab9c597d11f7b257e26
Joseph Spearritt <joe.spearritt@live.com>
2015-11-28 21:29:56 +1000

    Added simple http authentication

---
+++ charon/authentication.py
@@ -0,0 +1,21 @@
+from functools import wraps
+from flask import request, Response
+
+def check_auth(username, password):
+    """
+    Check the authorisation of a username and password
+
+    :param username: the username to check
+    :param password: the password to check
+    :return: true if auth successful, false otherwise
+    """
+    return username == "user" and password == "password"
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return Response('Login!', 401, {'WWW-Authenticate': 'Basic realm="Login!"'})
+        return f(*args, **kwargs)
+    return decorated
