df0f98d40478bd91cbced6169269394523ee598b
Joseph Spearritt <joe.spearritt@live.com>
2015-11-28 12:50:18 +1000

    Add terminal api beginnings

--- charon/__init__.py
+++ charon/__init__.py
@@ -7,8 +7,11 @@
 
 from flask import Flask
 from charon.mod_terminal import mod_terminal
+from charon.serversessions import MongoSession, MongoSessionInterface
 
 application = Flask(__name__)
 application.register_blueprint(mod_terminal, url_prefix='/terminal')
+application.config['terminal'] = {}
+application.session_interface = MongoSessionInterface(db='sessiondb')
 
 from . import views
