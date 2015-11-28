14b7267f9504eff5fd125d915516a0544888e27c
Thomas Cranny <Thom@sCranny.net>
2015-11-28 14:55:48 +1000

    Destroy all pymongo usages

--- charon/__init__.py
+++ charon/__init__.py
@@ -8,13 +8,11 @@
 from flask import Flask
 from .mod_terminal import mod_terminal
 from .mod_fileserver import mod_fileserver
-from .serversessions import MongoSession, MongoSessionInterface
 
 application = Flask(__name__)
 
 application.register_blueprint(mod_terminal, url_prefix='/terminal')
 application.config['terminal'] = {}
-application.session_interface = MongoSessionInterface(db='sessiondb')
 
 application.register_blueprint(mod_fileserver, url_prefix='/fs')
 
